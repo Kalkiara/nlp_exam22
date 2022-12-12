4f2a237750321f2b75a88fd96572f4e2955c98dd
miscodisco <mia.jacobsen@live.dk>
2022-12-12 13:09:02 +0100

    added doc strings

--- src/tools.py
+++ src/tools.py
@@ -7,13 +7,14 @@ import random
 import csv
 
 def load_tasks(file_name):
-    """_summary_
+    """creates a list of prompts or sentences with [MASK]-tokens based on a txt-file
 
     Args:
-        file_name (_type_): _description_
+        file_name (.txt file): .txt file containing the tasks given to LM model. 
+                                Each line should correspond to one task
 
     Returns:
-        _type_: _description_
+        a list 
     """
     data_path = os.path.join("data", file_name)
     with open(data_path) as f:
@@ -22,13 +23,13 @@ def load_tasks(file_name):
     return lines
 
 def gpt2_test(list_tasks):
-    """_summary_
+    """Text generation pipeline using GPT-2
 
     Args:
-        list_tasks (_type_): _description_
+        list_tasks (list): list of tasks created using load_tasks(). 
 
     Returns:
-        _type_: _description_
+        list: list of generated output words based on the prompts. 
     """
     gpt2_generator = pipeline('text-generation', model = 'gpt2')
     outputs = []
@@ -44,13 +45,13 @@ def gpt2_test(list_tasks):
     return outputs
 
 def gpt3_test(list_tasks):
-    """_summary_
+    """Text generation pipeline using GPT-3
 
     Args:
-        list_tasks (_type_): _description_
+        list_tasks (list): list of tasks created using load_tasks().
 
     Returns:
-        _type_: _description_
+        outputs (list): list of generated output words based on the prompts.
     """
     openai.api_key = "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"
 
@@ -72,6 +73,14 @@ def gpt3_test(list_tasks):
     return outputs 
 
 def bert_base_test(list_tasks):
+    """Fill masked token pipeline using BERT base, uncased. 
+
+    Args:
+        list_tasks (list): list of tasks created using load_tasks().
+
+    Returns:
+        list: list of generated tokens for each [MASK] in each task.
+    """
     bert_base_pipe = pipeline('fill-mask', model='bert-base-uncased')
     outputs = []
     for task in list_tasks:
@@ -81,6 +90,14 @@ def bert_base_test(list_tasks):
     return outputs
 
 def bert_large_test(list_tasks):
+    """Fill masked token pipeline using BERT large, uncased. 
+
+    Args:
+        list_tasks (list): list of tasks created using load_tasks().
+
+    Returns:
+        list: list of generated tokens for each [MASK] in each task.
+    """
     bert_base_pipe = pipeline('fill-mask', model='bert-large-uncased')
     outputs = []
     for task in list_tasks:
@@ -89,15 +106,18 @@ def bert_large_test(list_tasks):
         outputs.append(temp_out[0]['token_str'])
     return outputs
 
-def perform_test(list_tasks, model):
-    """_summary_
+def perform_test(list_tasks, model = 'gpt2'):
+    """Test model knowledge of color using a list of tasks and a given LM. 
 
     Args:
-        list_tasks (_type_): _description_
-        model (_type_): _description_
+        list_tasks (list): list of tasks created using load_tasks().
+        model (str): Name of an LM to test. Defaults to GPT-2
 
     Returns:
-        _type_: _description_
+        dictionary or str: 
+            if the specified model is one we have tested, 
+            a dictionary with tasks and the generated word for the given task is returned. 
+            if the specified model is not one we have tested, a string saying this is returned. 
     """
     if model == 'gpt2':
         outputs = gpt2_test(list_tasks)
@@ -122,10 +142,17 @@ def perform_test(list_tasks, model):
 
     return output
 
+def save_output(output, output_filename = 'output.csv'):
+    """saves output dictionary as a semi-colon separated .csv-file. 
 
-    
+    Args:
+        output (dictionary): the output dictionary created using perform_tests()
+        output_filename (str): the name of the output file. 
+                               Has to end with .csv, defaults to 'output.csv'
 
-def save_output(output, output_filename):
+    Returns:
+        None (instead, a file is saved in 'out' folder with the specified name)
+    """
     out_path = os.path.join("out", output_filename)
 
     with open(out_path, "w") as csv_file:
@@ -134,3 +161,5 @@ def save_output(output, output_filename):
         # write every key and value to file
             key = key.replace(' [MASK].', "")
             writer.writerow([key, val])
+
+    return None 
